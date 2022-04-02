d4ff57df8bccc4f88f7bd9d69226149173c679df
MaxWu <WuMax13@gmail.com>
2022-04-03 00:08:11 +0800

    AP325

---
+++ Q_4_8.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+using namespace std;
+typedef long long ll;
+int main(){
+    IOS
+    ll arr[200005];
+    int n, m; // 199487 48360
+    cin >> n >> m;
+    for(int i = 0;i < n;i++) cin >> arr[i];
+    priority_queue<ll, vector<ll>, greater<ll> > pq;
+    for(int i = 0;i < n;i++){
+        if(pq.size() < m){
+            pq.push(arr[i]);
+        }
+        else{
+            ll tmp = pq.top() + arr[i];
+            pq.pop();
+            pq.push(tmp);
+        }
+    }
+    for(int i = 0;i < m-1;i++) pq.pop();
+    cout << pq.top() << '\n';
+
+    return 0;
+}
