72220a7994471122ca0944bb4db8295476c02245
MaxWu <WuMax13@gmail.com>
2022-04-05 10:01:23 +0800

    AP325

--- Q_4_8.cpp
+++ Q_4_8.cpp
@@ -1,23 +1,26 @@
-// dynamic priority queue
 #include <bits/stdc++.h>
 #define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 using namespace std;
+typedef long long ll;
 int main(){
     IOS
-    int n, m;
-    priority_queue<pair<int, int>> pq;
-    priority_queue<int> counter[200005];
+    ll arr[200005];
+    int n, m; // 199487 48360
     cin >> n >> m;
+    for(int i = 0;i < n;i++) cin >> arr[i];
+    priority_queue<ll, vector<ll>, greater<ll> > pq;
     for(int i = 0;i < n;i++){
-        int tmp;
-        cin >> tmp;
-        pq.push(make_pair(-i, tmp));
+        if(pq.size() < m){
+            pq.push(arr[i]);
+        }
+        else{
+            ll tmp = pq.top() + arr[i];
+            pq.pop();
+            pq.push(tmp);
+        }
     }
-    for(int i = 0;i < n;i++){
-        pair<int, int> timing = pq.top();
-        pq.pop();
-        
-    }
-
+    for(int i = 0;i < m-1;i++) pq.pop();
+    cout << pq.top() << '\n';
+ 
     return 0;
 }
