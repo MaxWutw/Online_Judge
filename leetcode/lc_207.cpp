135a0613c8a72986303a38b7f4e9d834b3c2b34f
Max Wu <MaxWu13@gmail.com>
2022-09-17 23:45:04 +0800

    OJ

--- leetcode/lc_207.cpp
+++ leetcode/lc_207.cpp
@@ -1,7 +1,3 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-using namespace std;
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
@@ -30,26 +26,3 @@ private:
         return false;
     }
 };
-int main(){
-    solution = Solution();
-    vector<vector<int>> vec = {{1, 0}, {0, 1}};
-    solution.canFinish(2, vec);
-    
-    return 0;
-}
-
-// #include <iostream>
-// #include <vector>
-// using namespace std;
-// // void foo(int *a){
-// //     cout << a[5] << endl;
-// // }
-// int main(){
-//     // int a[10] = {1, 2, 3, 4, 5, 6, 7, 8 ,9};
-//     // foo(a);
-//     vector<vector<int>> vec;
-//     vec.push_back(.push_back(4);
-//     cout << vec[0][0] << endl;
-
-//     return 0;
-// }
