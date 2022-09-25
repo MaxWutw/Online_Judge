3d6477e202fb9abe3947840447675c69d03113c9
Max Wu <MaxWu13@gmail.com>
2022-09-26 00:35:23 +0800

    OJ

--- program.cpp
+++ program.cpp
@@ -1,32 +1,38 @@
-// disjoint set union-find algorithm
-#include <bits/stdc++.h>
-#define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#include <iostream>
 using namespace std;
-int p[1000005];
-int find(int x){
-    if(p[x] < 0) return x;
-    return p[x] = find(p[x]);
+class Parent{
+protected:
+    int data = 67855;
+private:
+    int pri = -10;
+public:
+    int pub = 100;
+    void print();
+    void number(int &a);
+};
+void Parent::print(){
+        cout << "Hello World" << endl;
 }
-int main(){
-    IOS
-    int n, m;
-    while(cin >> n >> m){
-        for(int i = 0;i < n + 5;i++) p[i] = -1;
-        int a, b;
-        int mx = 1;
-        for(int i = 0;i < m;i++){
-            cin >> a >> b;
-            a = find(a);
-            cout << p[a] << '\n';
-            b = find(b);
-            if(a != b){
-                p[a] += p[b];
-                mx = max(mx, -p[a]);
-                p[b] = a;
-            }
-        }
-        cout << mx << '\n';
+void Parent::number(int &a){
+    cout << a << endl;
+}
+class Child: public Parent{
+public:
+    void get_pro(){
+        cout << pub << endl;
     }
+    void get_data(){
+        cout << data << endl;
+    }
+    void get_pri(){
+        cout << pri << endl;
+    }
+};
+int main(){
+    Child c;
+    c.get_data();
+    c.get_pri();
+    c.get_pro();
 
     return 0;
 }
