a0ff8fe942fadb44b45e8d9ae7989b3996f73b8d
Max Wu <MaxWu13@gmail.com>
2022-10-17 00:09:02 +0800

    OJ

--- solve.cpp
+++ solve.cpp
@@ -1,77 +1,46 @@
 #include <bits/stdc++.h>
 #define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define lowbit(x) (x & -x)
 using namespace std;
 typedef long long ll;
-int n;
-ll arr[500005];
-ll c1[500005], c2[500005];
-class BIT{
-public:
-    // BIT(){
-    // }
-    // void init();
-    void update(int x, int k);
-    ll query(int x);
-    inline void range_update(int x, int y, int k){
-        this->update(x, k), this->update(y + 1, -k);
-    }
-    inline ll range_query(int x, int y){
-        return (this->query(y) - this->query(x - 1));
-    }
-private:
-    inline int lowbit(int x) { return (x & -x); };
-};
-// void BIT::init(){
-//     for(int i = 1;i <= n;i++){
-//         tree[i] += arr[i];
-//         int j = i + lowbit(i);
-//         if(j <= n) tree[j] += tree[i];
-//     }
-// }
-void BIT::update(int x, int k){
-    ll i = x;
+int n, m, tmp, tree[100005];
+void update(int x, int k){
     while(x <= n){
-        c1[x] += k;
-        c2[x] += k * (i - 1);
+        tree[x] += k;
         x += lowbit(x);
     }
 }
-ll BIT::query(int x){
-    ll res = 0, i = x;
+int query(int x){
+    int res = 0;
     while(x){
-        res += c1[x]*i - c2[x];
+        res += tree[x];
         x -= lowbit(x);
     }
     return res;
 }
+vector<ll> vec1, vec2;
 int main(){
     IOS
-    cin >> n;
-    BIT bit;
-    memset(arr, 0, sizeof(arr));
-    memset(c1, 0, sizeof(c1));
-    memset(c2, 0, sizeof(c2));
-    for(int i = 1;i <= n;i++){
-        cin >> arr[i];
-        bit.update(i, arr[i] - arr[i - 1]);
-    }
-    int q;
-    cin >> q;
-    while(q--) {
-        int v, x, y, k;
-        cin >> v;
-        if(v == 1){
-            cin >> x >> y >> k;
-            // bit.update(x, y, k);
-            bit.range_update(x, y, k);
-        } 
-        else{
-            cin >> x >> y;
-            // cout << bit.query(x, y) << '\n';
-            cout << bit.range_query(x, y) << '\n';
+    while(cin >> n){
+        memset(tree, 0, sizeof(tree));
+        vec1.clear();
+        vec2.clear();
+        for(int i = 1;i <= n;i++){
+            cin >> tmp;
+            vec1.push_back(tmp);
+            vec2.push_back(tmp);
         }
-
+        sort(vec2.begin(), vec2.end());
+        for(int i = 1;i <= n;i++){
+            vec1[i] = lower_bound(vec2.begin(), vec2.end(), vec1[i]) - vec2.begin() + 1;
+        }
+        // for(int i = 0;i < n;i++) cout << vec1[i] << ' ';
+        for(int i = 1;i <= n;i++){
+            update(vec1[i], 1);
+            cout << i - query(vec1[i]) + 1 << '\n';
+        }
     }
+    
 
     return 0;
 }
