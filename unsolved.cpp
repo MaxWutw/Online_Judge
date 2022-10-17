519989ee0a93dfa7173702fee049f10a8e30dffb
Max Wu <MaxWu13@gmail.com>
2022-10-18 00:24:41 +0800

    OJ

--- unsolved.cpp
+++ unsolved.cpp
@@ -2,25 +2,9 @@
 #define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 using namespace std;
 typedef long long ll;
-int n, total, arr[20];
-bool visited[100];
-int dfs(int depth){
-    if(depth > n){
-        total++;
-        for(int i = 0;i < n;i++){
-            for(int j = 0;j < n;j++){
-                cout << 
-            }
-        }
-    }
-}
 int main(){
     IOS
     
-    while(cin >> n && n){
-        for(int i = 0;i < n;i++) arr[i] = i;
-        dfs(1);
-    }
 
     return 0;
 }
---
+++ zerojudge/zj_a290.cpp
@@ -0,0 +1,36 @@
+// dfs + adjencency list
+#include <bits/stdc++.h>
+#define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+using namespace std;
+typedef long long ll;
+int n, m, x, y;
+bool visited[10005];
+vector<int> adj[10005];
+void dfs(int depth){
+    for(int i : adj[depth]){
+        if(visited[i]) continue;
+        visited[i] = true;
+        dfs(i);
+    }
+}
+int main(){
+    IOS
+    while(cin >> n >> m){
+        memset(visited, 0, sizeof(visited));
+        for(int i = 0;i < m;i++){
+            int a, b;
+            cin >> a >> b;
+            if(a == b) continue;
+            a--, b--;
+            adj[a].push_back(b);
+        }
+        cin >> x >> y;
+        x--, y--;
+        dfs(x);
+        cout << (visited[y] ? "Yes!!!\n" : "No!!!\n");
+        for(int i = 0;i < n;i++)
+            adj[i].clear();
+    }
+
+    return 0;
+}
