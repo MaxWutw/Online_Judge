3a02be3c5c2ba7627148bf3b994e9ac0c3251450
Max Wu <MaxWu13@gmail.com>
2022-10-22 22:40:01 +0800

    OJ

--- solve2.cpp
+++ solve2.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
 #define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 using namespace std;
+typedef long long ll;
 int main(){
     IOS
-    float f = 1.2373845563;
-    cout << fixed << setprecision(7) << f;
+    
 
     return 0;
 }
--- zerojudge/zj_a454.cpp
+++ zerojudge/zj_a454.cpp
@@ -1,4 +1,4 @@
-// DAG, topological sort
+// DAG dp, topological sort
 #include <bits/stdc++.h>
 #define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 using namespace std;
