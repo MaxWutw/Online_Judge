9587f387b3ee424e437b04e0e410cb452c6cca1c
Max Wu <MaxWu13@gmail.com>
2022-10-29 20:14:42 +0800

    OJ

--- unsolved.cpp
+++ unsolved.cpp
@@ -1,44 +1,33 @@
-// SPFA
 #include <bits/stdc++.h>
 #define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 #define INF 0x3f3f3f3f
 using namespace std;
 typedef long long ll;
-int n, m;
-vector<pair<int, int> > adj[500005];
-int dis[500005];
-bool inque[500005];
 int main(){
     IOS
-    cin >> n >> m;
-    memset(dis, INF, sizeof(dis));
-    for(int i = 0;i < m;i++){
-        int u, v, w;
-        cin >> u >> v >> w;
-        u--, v--;
-        adj[u].push_back({v, w});
-        // adj[u].push_back({v, w});
-    }
-    queue<int> q;
-    memset(inque, 0, sizeof(inque));
-    inque[0] = true;
-    dis[0] = 0; 
-    q.push(0);
-    while(!q.empty()){
-        int cur = q.front();
-        q.pop();
-        inque[cur] = false;
-        for(auto e : adj[cur]){
-            if(dis[e.first] > dis[cur] + e.second){
-                dis[e.first] = dis[cur] + e.second;
-                if(!inque[e.first]){
-                    inque[e.first] = true;
-                    q.push(e.first);
+    int n;
+    while(cin >> n){
+        int dp[30][30];
+        memset(dp, 0, sizeof(dp));
+        while(n--){
+            char a, b;
+            int t;
+            cin >> a >> b >> t;
+            dp[(int)a - 65][(int)b - 65] = t;
+            dp[(int)b - 65][(int)a - 65] = t;
+        }
+        char s, e;
+        cin >> s >> e;
+        for(int k = 0;k < 26;k++){
+            for(int i = 0;i < 26;i++){
+                for(int j = 0;j < 26;j++){
+                    dp[i][j] = min(dp[i][k] + dp[k][j], dp[i][j]);
                 }
             }
         }
+        
+        cout << dp[(int)s - 65][(int)e - 65] << '\n';
     }
-    cout << dis[n - 1] << '\n';
 
     return 0;
 }
