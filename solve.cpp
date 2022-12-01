ef3c3731e628f2336dc0f0327255d8f19be179ea
Max Wu <MaxWu13@gmail.com>
2022-12-02 00:27:03 +0800

    OJ

--- solve.cpp
+++ solve.cpp
@@ -4,12 +4,21 @@
 using namespace std;
 typedef long long ll;
 int v[10000], k[10000];
-int dp[2][10000];
+int dp[10000];
 int main(){
     IOS
     int n;
     while(cin >> n){
+        memset(dp, 0, sizeof(dp));
+        for(int i = 0;i < n;i++)
+            cin >> v[i] >> k[i];
 
+        for(int i = 0;i < n;i++){
+            for(int j = 100;j >= v[i];j--){
+                dp[j] = max(dp[j], dp[j - v[i]] + k[i]);
+            }
+        }
+        cout << dp[100] << '\n';
     }
 
     return 0;
