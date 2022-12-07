a856ab1283d704dbdc20940423a64c9254dd79ea
WuMaxtw <WuMax13@gmail.com>
2022-12-07 17:48:45 +0800

    OJ

--- solve2.cpp
+++ solve2.cpp
@@ -3,19 +3,21 @@
 #define INF 0x3f3f3f3f
 using namespace std;
 typedef long long ll;
-const ll MOD = 1000000007;
-ll fp(ll a, ll b){
-    int ans = 1;
-    while(b > 0){
-        if(b & 1) ans = ans * a % MOD;
-        a = a * a % MOD;
-        b >>= 1;
-    }
-    return ans;
-}
+pair<double, double> p[15];
 int main(){
     IOS
-    cout << fp(2584, 365435296162) << '\n';
+    int n;
+    while(cin >> n){
+        for(int i = 0;i < n;i++){
+            cin >> p[i].first >> p[i].second;
+        }
+        p[n] = p[0];
+        double area = 0.0;
+        for(int i = 0;i < n;i++){
+            area += p[i].first * p[i + 1].second - p[i].second * p[i + 1].first;
+        }
+        cout << fixed << setprecision(2) << fabs(area / 2.0) << '\n';
+    }
 
     return 0;
 }
