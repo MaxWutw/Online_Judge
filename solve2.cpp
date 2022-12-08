bc880cc80e5ffe523490f2dc7565520143cf9e3e
WuMaxtw <WuMax13@gmail.com>
2022-12-08 12:27:17 +0800

    OJ

--- solve2.cpp
+++ solve2.cpp
@@ -3,21 +3,17 @@
 #define INF 0x3f3f3f3f
 using namespace std;
 typedef long long ll;
-pair<double, double> p[15];
+vector<pair<double, double>> vec;
 int main(){
     IOS
-    int n;
-    while(cin >> n){
-        for(int i = 0;i < n;i++){
-            cin >> p[i].first >> p[i].second;
-        }
-        p[n] = p[0];
-        double area = 0.0;
-        for(int i = 0;i < n;i++){
-            area += p[i].first * p[i + 1].second - p[i].second * p[i + 1].first;
-        }
-        cout << fixed << setprecision(2) << fabs(area / 2.0) << '\n';
+    int n, a;
+    cin >> n >> a;
+    for(int i = 0;i < n;i++){
+        double x, y;
+        cin >> x >> y;
+        vec.emplace_back(x, y);
     }
+    sort(vec.begin(), vec.end());
 
     return 0;
 }
