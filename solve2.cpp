51f5e9fb885a351c9fe37273a510c79df04fa96e
WuMaxtw <WuMax13@gmail.com>
2022-12-28 00:50:28 +0800

    OJ

--- solve2.cpp
+++ solve2.cpp
@@ -3,19 +3,9 @@
 #define INF 0x3f3f3f3f
 using namespace std;
 typedef long long ll;
-const ll MOD = 1000000007;
-ll fp(ll a, ll b){
-    int ans = 1;
-    while(b > 0){
-        if(b & 1) ans = ans * a % MOD;
-        a = a * a % MOD;
-        b >>= 1;
-    }
-    return ans;
-}
 int main(){
     IOS
-    cout << fp(2584, 365435296162) << '\n';
+    cout << (1 << 5) << '\n';
 
     return 0;
 }
