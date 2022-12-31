1244798548f7a6b32c72186875461904c8ae1fe8
WuMaxtw <WuMax13@gmail.com>
2022-12-31 13:57:11 +0800

    OJ

--- solve.cpp
+++ solve.cpp
@@ -3,18 +3,7 @@
 using namespace std;
 int main(){
     IOS
-    int t;
-    // cin >> t;
-    string str;
-    cin >> str;
-    for(int i = 0;i < t;i++){
-        if((int)str[i] > 90) 
-            str[i] -= (1 << 5);
-        else
-            str[i] += (1 << 5);
-    }
-    for(int i = 0;i < t;i++) cout << str[i];
-    cout << '\n';
+    
 
     return 0;
 }
