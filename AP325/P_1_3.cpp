a6fa5f1d1b16c40c77eba942d09d336fae4f1984
WuMaxtw <WuMax13@gmail.com>
2023-01-01 01:27:19 +0800

    OJ

--- AP325/P_1_3.cpp
+++ AP325/P_1_3.cpp
@@ -1,16 +1,17 @@
+// recursion
 #include <bits/stdc++.h>
 #define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 using namespace std;
 typedef long long ll;
 ll n, L, arr[50005];
-int call(ll l, ll r){
+ll call(ll l, ll r){
     if(r - l <= 1) return 0;
     double mid = (arr[l] + arr[r]) >> 1;
     ll leng = arr[r] - arr[l];
     ll idx = l;
     while(arr[idx] < mid) idx++;
     if(arr[idx - 1] - arr[l] >= arr[r] - arr[idx]) idx--;
-    return (ll)leng + call(l, idx) + call(idx, r);
+    return leng + call(l, idx) + call(idx, r);
 }
 int main(){
     IOS
