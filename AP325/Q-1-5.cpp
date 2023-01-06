43b03fa1f3b24825fd5d5d9775a0e6d8ffd50c04
WuMaxtw <WuMax13@gmail.com>
2023-01-07 00:13:29 +0800

    OJ

--- AP325/Q-1-5.cpp
+++ AP325/Q-1-5.cpp
@@ -1,3 +1,4 @@
+// recursion
 #include <bits/stdc++.h>
 #define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 using namespace std;
--- solve2.cpp
+++ solve2.cpp
@@ -5,7 +5,6 @@ using namespace std;
 typedef long long ll;
 int main(){
     IOS
-    cout << (1 << 5) << '\n';
 
     return 0;
 }
