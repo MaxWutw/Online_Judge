7a9114b56a28574ca98cee72341cad7dae69b94b
WuMaxtw <WuMax13@gmail.com>
2023-01-08 09:03:25 +0800

    OJ

--- AP325/P_4_11.cpp
+++ AP325/P_4_11.cpp
@@ -1,4 +1,4 @@
-// sweep line
+// version 1: sweep line
 #include <bits/stdc++.h>
 #define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 using namespace std;
@@ -29,5 +29,31 @@ int main(){
     total += (last.second - last.first);
     cout << total << '\n';
     
+    return 0;
+}
+
+// version 2: dp
+#include <bits/stdc++.h>
+#define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+using namespace std;
+typedef long long ll;
+ll arr[100005], dp[100005];
+int main(){
+    IOS
+    ll n;
+    cin >> n;
+    for(int i = 0;i < n;i++)
+        cin >> arr[i];
+    dp[0] = arr[0];
+    for(int i = 1;i < n;i++)
+        dp[i] = max(dp[i - 1] + arr[i], arr[i]);
+    int m = 0;
+    for(int i = 1;i < n;i++){
+        if(dp[i] > dp[m]){
+            m = i;
+        }
+    }
+    cout << dp[m] << '\n';
+
     return 0;
 }
