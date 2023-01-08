c54c3af16908507df83bed4721b73d3c93f4bec5
WuMaxtw <WuMax13@gmail.com>
2023-01-08 13:50:08 +0800

    OJ

--- AP325/P_1_7.cpp
+++ AP325/P_1_7.cpp
@@ -3,21 +3,22 @@
 using namespace std;
 typedef long long ll;
 const int mod = 10009;
-ll arr[20], ans = 0, n;
+ll arr[30], ans = 0, n;
 void rec(int num, int cnt){
     if(cnt >= n){
         if(num == 1) ans++;
         return;
     }
-    rec((num * arr[cnt]) % mod, ++cnt);
-    rec(num, ++cnt);
+    rec((num * arr[cnt]) % mod, cnt + 1);
+    rec(num, cnt + 1);
+    return;
 }
 int main(){
     IOS
     cin >> n;
     for(int i = 0;i < n;i++) cin >> arr[i];
     rec(1, 0);
-    cout << ans << '\n';
+    cout << ans - 1 << '\n';
 
     return 0;
 }
