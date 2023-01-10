dcd4375c0d3647365deeae25d317ef6a1487d54c
WuMaxtw <WuMax13@gmail.com>
2023-01-11 00:45:09 +0800

    OJ

--- solve.cpp
+++ solve.cpp
@@ -3,25 +3,9 @@
 #define INf 0x3f3f3f3f
 using namespace std;
 typedef long long ll;
-int n, q, arr[200005];
 int main(){
     IOS
-    cin >> n >> q;
-    for(int i = 0;i < n;i++) cin >> arr[i];
-    int idx = 0;
-    while(q--){
-        int t, a, b;
-        cin >> t >> a >> b;
-        if(t == 1){
-            swap(arr[(a + idx - 1) % n], arr[(b + idx - 1) % n]);
-        }
-        else if(t == 2){
-            idx = (idx - 1 + n) % n;
-        }
-        else if(t == 3){
-            cout << arr[(a + idx - 1) % n] << '\n';
-        }
-    }
+    
 
     return 0;
 }
