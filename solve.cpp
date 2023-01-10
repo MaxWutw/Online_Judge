0dfbe874e99ab490d06156d5317b119eb58fbb9b
WuMaxtw <WuMax13@gmail.com>
2023-01-11 01:04:56 +0800

    OJ

--- solve.cpp
+++ solve.cpp
@@ -3,9 +3,21 @@
 #define INf 0x3f3f3f3f
 using namespace std;
 typedef long long ll;
+int n, q, arr[100005], dif[100005];
 int main(){
     IOS
-    
+    cin >> n >> q;
+    for(int i = 0;i < n;i++) cin >> arr[i];
+    dif[0] = arr[0];
+    for(int i = 1;i < n;i++){
+        dif[i] = arr[i] - arr[i - 1];
+    }
+    while(q--){
+        int l, r, v;
+        cin >> l >> r >> v;
+        dif[l] += v;
+        dif[r + 1] -= v;
+    }
 
     return 0;
 }
--- solve2.cpp
+++ solve2.cpp
@@ -2,23 +2,10 @@
 #define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 using namespace std;
 typedef long long ll;
-int n, arr[20], pos[20];
-ll ans = -2147483647;
-int queen(int k){
-    if(k >= n) return 1;
-    for(int i = 0;i < n;i++){
-        for(int j = 0;j < k;j++){
-            if(pos[j] == i || )
-        }
-    }
-}
 int main(){
     IOS
-    memset(pos, 0, sizeof(pos));
-    cin >> n;
-    for(int i = 0;i < n;i++) cin >> arr[i];
-    queen(n);
-    cout << ans << '\n';
+    int a = -10;
+    cout << 10 - a << '\n';
 
     return 0;
 }
