e30fe0a8aa951e267b6fcf66c37f1053b4c82609
WuMaxtw <WuMax13@gmail.com>
2023-01-12 00:50:47 +0800

    OJ

--- solve.cpp
+++ solve.cpp
@@ -3,20 +3,18 @@
 #define INf 0x3f3f3f3f
 using namespace std;
 typedef long long ll;
-int n, q, arr[100005], dif[100005];
+int m, n, arr[200005];
+map<string, int> m;
 int main(){
     IOS
-    cin >> n >> q;
-    for(int i = 0;i < n;i++) cin >> arr[i];
-    dif[0] = arr[0];
-    for(int i = 1;i < n;i++){
-        dif[i] = arr[i] - arr[i - 1];
+    cin >> m >> n;
+    for(int i = 0;i < n;i++){
+        string str;
+        cin >> str;
+        m[str] = i;
     }
-    while(q--){
-        int l, r, v;
-        cin >> l >> r >> v;
-        dif[l] += v;
-        dif[r + 1] -= v;
+    for(int i = 0;i < n;i++){
+        
     }
 
     return 0;
