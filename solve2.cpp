88b97d1e3b0e2cea64e3c0a3231c3f1a0a693fb6
WuMaxtw <WuMax13@gmail.com>
2023-02-11 11:42:46 +0800

    OJ

--- solve2.cpp
+++ solve2.cpp
@@ -2,13 +2,9 @@
 #define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 using namespace std;
 typedef long long ll;
-void foo(int (&arr) ){
-    for(int i = 0;i < 5;i++) cout << arr[i] << ' ';
-}
+vector<ll> a(1 << 20), b(1 << 20);
 int main(){
     IOS
-    int a[10] = {1, 2, 3, 5, 7};
-    foo(a);
 
     return 0;
 }
