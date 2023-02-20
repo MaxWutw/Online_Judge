3f555c53c437f6308e682e6502fdf0b37c0ffe94
WuMaxtw <WuMax13@gmail.com>
2023-02-20 23:43:31 +0800

    OJ

--- solve.cpp
+++ solve.cpp
@@ -3,24 +3,13 @@
 #define INF 0x3f3f3f3f
 using namespace std;
 typedef long long ll;
-int arr[200005], cnt[200005];
+int arr[200005];
 int main(){
     IOS
-    int n;
-    memset(cnt, 0, sizeof(cnt));
-    cin >> n;
+    int m, n;
+    cin >> m >> n;
     for(int i = 0;i < n;i++) cin >> arr[i];
-    int l = 0, r = 0, best = 0, cur = 0;
-    while(r < n){
-        cnt[arr[r]]++;;
-        while(cnt[arr[r]] > 1){
-            cnt[arr[l]]--;
-            l++;
-        }
-        best = max(best, r - l);
-        r++;
-    }
-    cout << best + 1 << '\n';
+    
 
     return 0;
 }
