11c28e1fb833551f5e779a278211b358e68b2585
WuMaxtw <WuMax13@gmail.com>
2023-03-06 00:28:33 +0800

    OJ

--- solve.cpp
+++ solve.cpp
@@ -5,15 +5,26 @@ using namespace std;
 typedef long long ll;
 struct Data{
     int m, s, t;
+};
+bool cmp(Data &a, Data &b){
+    return a.s < b.s;
 }
 int main(){
     IOS
     int n;
-    pair<int, int> tp[100005];
+    Data tp[100005];
     cin >> n;
-    for(int i = 0;i < n;i++) cin >> tp[i].first;
-    for(int i = 0;i < n;i++) cin >> tp[i].second;
-
+    for(int i = 0;i < n;i++) cin >> tp[i].m;
+    for(int i = 0;i < n;i++) cin >> tp[i].s;
+    for(int i = 0;i < n;i++) cin >> tp[i].t;
+    sort(tp, tp + n, cmp);
+    ll max_p = 0;
+    Data last = tp[0];
+    for(int i = 1;i < n;i++){
+        if(last.t < tp[i].s) max_p = max(max_p, last.m);
+        
+    }
+    cout << max_p << '\n';
 
     return 0;
 }
