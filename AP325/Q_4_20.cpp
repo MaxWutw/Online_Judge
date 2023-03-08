4cae404351847fcd0e0ccbafb9d2621c3b4d2644
WuMaxtw <WuMax13@gmail.com>
2023-03-08 13:15:36 +0800

    OJ

--- AP325/Q_4_20.cpp
+++ AP325/Q_4_20.cpp
@@ -33,3 +33,39 @@ int main(){
 
     return 0;
 }
+
+#include <bits/stdc++.h>
+#define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+using namespace std;
+typedef long long ll;
+int main(){
+    IOS
+    int n, x, y;
+    pair<int, int> tp[100005];
+    cin >> n;
+    cin >> x >> y;
+    for(int i = 0;i < n;i++) cin >> tp[i].first >> tp[i].second;
+    sort(tp, tp + n);
+    stack<pair<int, int> > s;
+    bool judge = true;
+    int s = 0, t = 0;
+    while(s < n && tp[s].second < x) s++;
+    while(t < n && tp[t].first < y) t++;
+    if(tp[s + 1].first > x || tp[t].second < y){
+        cout << -1 << '\n';
+        return 0;
+    }
+    s.push(tp[s]);
+    for(int i = s + 1;i < t;i++){
+        if(s.top().second < tp[i].first){
+            judge = false;
+            break;
+        }
+        if(s.top().second >= tp[i].second) continue;
+        s.push(tp[i]);
+        if(s.top().second < tp[i].first) s.pop();
+    }
+    cout << (judge ? s.size() : -1) << '\n';
+
+    return 0;
+}
