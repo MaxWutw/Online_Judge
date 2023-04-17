fad9faaf0970e1c9734f83b59175305dfe0964a7
WuMaxtw <WuMax13@gmail.com>
2023-04-17 16:29:50 +0800

    OJ

--- solve.cpp
+++ solve.cpp
@@ -6,25 +6,11 @@ typedef long long ll;
 int main(){
     IOS
     int n, x;
-    pair<ll, ll> arr[5005];
     cin >> n >> x;
-    for(int i = 0;i < n;i++){
-        cin >> arr[i].first;
-        arr[i].second = i + 1;
-    }
-    sort(arr, arr + n);
-    for(int i = 0;i < n;i++){
-        int l = 0, r = n - 1;
-        while(l != r){
-            if(l != i && r != i && (x - arr[i].first) == (arr[l].first + arr[r].first)){
-                cout << arr[l].second << ' ' << arr[i].second << ' ' << arr[r].second << '\n';
-                return 0;
-            }
-            if((x - arr[i].first) < (arr[l].first + arr[r].first)) r--;
-            else l++;
-        }
-    }
-    cout << "IMPOSSIBLE" << '\n';
+    // pair<ll, ll> arr[1005];
+    ll arr[1005];
+    for(int i = 0;i < n;i++) cin >> arr[i];
+    
 
     return 0;
 }
