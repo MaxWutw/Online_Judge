df5aad9a7325fb498fe24de36bc5460d66cca043
MaxWutw <WuMax13@gmail.com>
2023-04-26 21:19:20 +0800

    OJ

--- solve.cpp
+++ solve.cpp
@@ -2,8 +2,14 @@
 #define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 using namespace std;
 typedef long long ll;
+pair<ll, ll> arr[100005];
 int main(){
     IOS
+    int n;
+    cin >> n;
+    for(int i = 0;i < n;i++)
+        cin >> arr[i].first >> arr[i].second;
+    sort()
 
     return 0;
 }
--- test.cpp
+++ test.cpp
@@ -1,10 +1,41 @@
-#include <bits/stdc++.h>
-#define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-using namespace std;
-typedef long long ll;
-int main(){
-    IOS
-    
-    
-    return 0;
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+  float BMI,a,b;
+  int c;
+  c=1;
+  while (c==1){
+    printf("請輸入你的體重:");
+    scanf("%f",&a);
+    printf("請輸入你的身高:");
+    scanf("%f",&b);
+    b=b/100.0;
+    BMI=a/(b*b);
+    printf("你的BMI:%f\n",BMI);
+    if (BMI>=35){
+      printf("你的BMI是%f屬於重度肥胖\n",BMI);
+    }
+    else if (BMI>=30 && BMI<35){
+      printf("你的BMI是%f屬於中度肥胖\n",BMI);
+    }
+    else if (BMI>=27 && BMI<30){
+      printf("你的BMI是%f屬於輕度肥胖\n",BMI);
+    }
+    else if (BMI>=24 && BMI<27){
+      printf("你的BMI是%f屬於過重\n",BMI);
+    }
+    else if (BMI>=18.5 && BMI<24){
+      printf("你的BMI是%f屬於康體位\n",BMI);
+    }
+    else if (BMI<18.5){
+      printf("你的BMI是%f屬於過輕\n",BMI);
+    }
+    printf("再次使用請輸入1不使用輸入0");
+    scanf("%d",c);
+    if(c==0){
+      break;
+    }
+  }
+  return 0;
 }
