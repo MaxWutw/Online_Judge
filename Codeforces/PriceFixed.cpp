797fc9ed47a25b514c2f24e557ef36c243023a02
WuMaxtw <WuMax13@gmail.com>
2023-04-27 11:58:34 +0800

    OJ

--- Codeforces/PriceFixed.cpp
+++ Codeforces/PriceFixed.cpp
@@ -1,1 +1,44 @@
 // greedy + deque
+#include <bits/stdc++.h>
+#define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+using namespace std;
+typedef long long ll;
+pair<ll, ll> arr[100005];
+int main(){
+    IOS
+    int n;
+    pair<ll, ll> arr[100005];
+    deque<ll> dq;
+    cin >> n;
+    for(int i = 0;i < n;i++)
+        cin >> arr[i].first >> arr[i].second;
+    sort(arr, arr + n, [&](pair<ll, ll> a, pair<ll, ll> b){return a.second > b.second;});
+    for(int i = 0;i < n;i++)
+        dq.push_front(i);
+    ll cnt = 0, ans = 0;
+    while(!dq.empty()){
+        if(!dq.empty() && arr[dq.front()].first <= 0) dq.pop_front();
+        if(!dq.empty() && arr[dq.back()].first <= 0) dq.pop_back();
+        if(dq.empty()) break;
+        if(cnt >= arr[dq.front()].second){
+            ans += arr[dq.front()].first;
+            cnt += arr[dq.front()].first;
+            dq.pop_front();
+        }
+        else{
+            if(arr[dq.back()].first + cnt >= arr[dq.front()].second){
+                arr[dq.back()].first -= (arr[dq.front()].second - cnt);
+                ans += (2 * (arr[dq.front()].second - cnt));
+                cnt += (arr[dq.front()].second - cnt);
+            } 
+            else{
+                ans += (2 * arr[dq.back()].first);
+                cnt += arr[dq.back()].first;
+                dq.pop_back();
+            }
+        }
+    }
+    cout << ans << '\n';
+
+    return 0;
+}
--- solve.cpp
+++ solve.cpp
@@ -2,42 +2,9 @@
 #define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 using namespace std;
 typedef long long ll;
-pair<ll, ll> arr[100005];
 int main(){
     IOS
-    int n;
-    pair<ll, ll> arr[100005];
-    deque<ll> dq;
-    cin >> n;
-    for(int i = 0;i < n;i++)
-        cin >> arr[i].first >> arr[i].second;
-    sort(arr, arr + n, [&](pair<ll, ll> a, pair<ll, ll> b){return a.second > b.second;});
-    for(int i = 0;i < n;i++)
-        dq.push_front(i);
-    ll cnt = 0, ans = 0;
-    while(!dq.empty()){
-        if(!dq.empty() && arr[dq.front()].first <= 0) dq.pop_front();
-        if(!dq.empty() && arr[dq.back()].first <= 0) dq.pop_back();
-        if(dq.empty()) break;
-        if(cnt >= arr[dq.front()].second){
-            ans += arr[dq.front()].first;
-            cnt += arr[dq.front()].first;
-            dq.pop_front();
-        }
-        else{
-            if(arr[dq.back()].first >= arr[dq.front()].second - cnt){
-                arr[dq.back()].first -= (arr[dq.front()].second - cnt);
-                ans += (2 * (arr[dq.front()].second - cnt));
-                cnt += (arr[dq.front()].second - cnt);
-            } 
-            else{
-                ans += (2 * (arr[dq.back()].first - cnt));
-                cnt += (arr[dq.back()].first - cnt);
-                arr[dq.back()].first -= (arr[dq.back()].first - cnt);
-            }
-        }
-    }
-    cout << ans << '\n';
+
 
     return 0;
 }
