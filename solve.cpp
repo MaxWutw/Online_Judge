47b335115950fdff2babc125ad5581fedf237951
MaxWutw <WuMax13@gmail.com>
2023-05-05 00:21:27 +0800

    OJ

--- solve.cpp
+++ solve.cpp
@@ -2,36 +2,15 @@
 #define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 using namespace std;
 typedef long long ll;
-ll arr[200005];
+void divide(int l, int r, int n){
+    int mid = (l + r) >> 1;
+    
+}
 int main(){
-    IOS
-    int n;
+    int n, arr[200005];
     cin >> n;
     for(int i = 0;i < n;i++) cin >> arr[i];
-    priority_queue<ll, vector<ll>, greater<ll> > pq;
-    int ans = 0;
-    ll cur = 0;
-    for(int i = 0;i < n;i++){
-        if(arr[i] > 0){
-            cur += arr[i];
-            // cout << cur << '\n';
-            ans++;
-        }
-        else if(cur + arr[i] >= 0){
-            ans++;
-            cur += arr[i];
-            pq.push(arr[i]);
-        }
-        else{
-            if(!pq.empty() && pq.top() < arr[i]){
-                cur -= pq.top();
-                pq.pop();
-                cur += arr[i];
-                pq.push(arr[i]);
-            }
-        }
-    }
-    cout << ans << '\n';
+    divide(0, n - 1, n);
 
     return 0;
 }
