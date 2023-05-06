9df176daaa8d450fc03b27f919e172e965ac2d14
MaxWutw <WuMax13@gmail.com>
2023-05-06 22:27:34 +0800

    OJ

--- solve.cpp
+++ solve.cpp
@@ -2,15 +2,7 @@
 #define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 using namespace std;
 typedef long long ll;
-void divide(int l, int r, int n){
-    int mid = (l + r) >> 1;
-    
-}
 int main(){
-    int n, arr[200005];
-    cin >> n;
-    for(int i = 0;i < n;i++) cin >> arr[i];
-    divide(0, n - 1, n);
 
     return 0;
 }
