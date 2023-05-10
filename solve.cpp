f72994c58622f80bb06079b20756ccec84bbaddf
MaxWutw <WuMax13@gmail.com>
2023-05-10 22:29:43 +0800

    OJ

--- solve.cpp
+++ solve.cpp
@@ -2,17 +2,22 @@
 #define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 using namespace std;
 typedef long long ll;
+const ll mod = 1e9 + 7;
 int main(){
     IOS
-    int n, arr[100005];
-    int dp[100005];
+    int n, arr[1005][1005];
+    int dp[1005][1005];
     cin >> n;
-    for(int i = 0;i < n;i++) cin >> arr[i];
-    dp[0] = 0, dp[1] = abs(arr[1] - arr[0]);
-    for(int i = 2;i < n;i++){
-        dp[i] = min(dp[i - 1] + abs(arr[i] - arr[i - 1]), dp[i - 2] + abs(arr[i] - arr[i - 2]));
+    for(int i = 0;i < n;i++){
+        for(int j = 0;j < n;j++){
+            cin >> arr[i][j];
+        }
     }
-    cout << dp[n - 1] << '\n';
+    for(int i = 0;i < n;i++){
+        for(int j = 0;j < n;j++){
+            
+        }
+    }
 
     return 0;
 }
