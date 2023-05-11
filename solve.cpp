8b9d46b114d445922766f35ad84b82fc110b0b7d
WuMaxtw <WuMax13@gmail.com>
2023-05-11 09:54:41 +0800

    OJ

--- solve.cpp
+++ solve.cpp
@@ -2,36 +2,8 @@
 #define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 using namespace std;
 typedef long long ll;
-ll arr[200005];
 int main(){
     IOS
-    int n;
-    cin >> n;
-    for(int i = 0;i < n;i++) cin >> arr[i];
-    priority_queue<ll, vector<ll>, greater<ll> > pq;
-    int ans = 0;
-    ll cur = 0;
-    for(int i = 0;i < n;i++){
-        if(arr[i] > 0){
-            cur += arr[i];
-            // cout << cur << '\n';
-            ans++;
-        }
-        else if(cur + arr[i] >= 0){
-            ans++;
-            cur += arr[i];
-            pq.push(arr[i]);
-        }
-        else{
-            if(!pq.empty() && pq.top() < arr[i]){
-                cur -= pq.top();
-                pq.pop();
-                cur += arr[i];
-                pq.push(arr[i]);
-            }
-        }
-    }
-    cout << ans << '\n';
 
     return 0;
 }
