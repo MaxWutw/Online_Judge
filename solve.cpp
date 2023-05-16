c1d49181674bd1adf4cb1d55c399830660ff7793
WuMaxtw <WuMax13@gmail.com>
2023-05-16 12:51:48 +0800

    OJ

--- solve.cpp
+++ solve.cpp
@@ -2,20 +2,11 @@
 #define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 using namespace std;
 typedef long long ll;
-const ll mod = 1e9 + 7;
-ll dp[1000005];
 int main(){
     IOS
-    ll n;
-    cin >> n;
-    dp[0] = 1;
-    for(ll i = 1;i <= n;i++){
-        for(ll j = 1;j <= min(i, 6LL);j++){
-            dp[i] += dp[i - j];
-            dp[i] %= mod;
-        }
-    }
-    cout << dp[n] << '\n';
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << '\n';
 
     return 0;
 }
