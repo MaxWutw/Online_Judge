d325ee41395a829ce531a30d0bb3bf5e2b70ab7b
WuMaxtw <WuMax13@gmail.com>
2023-05-28 23:07:19 +0800

    OJ

--- solve.cpp
+++ solve.cpp
@@ -2,43 +2,8 @@
 #define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 using namespace std;
 typedef long long ll;
-int n, m, p, k;
-vector<pair<int, int> > mem[20005], ret[20005];
-bool vis[20005];
-int team[20005];
-void dfs(int st){
-    vis[st] = true;
-    for(auto e : mem[st]){
-        dfs(e);
-    }
-}
-int check(){
-
-}
 int main(){
     IOS
-    cin >> n >> m;
-    for(int i = 0;i < m;i++){
-        int a, b;
-        cin >> a >> b;
-        mem[a].push_back({b, 0});
-        mem[b].push_back({a, 0});
-    }
-    cin >> p >> k;
-    for(int i = 0;i < p;i++){
-        for(int j = 0;j < k;j++){
-            int a, b;
-            cin >> a >> b;
-            ret[a].push_back({b, i});
-            ret[b].push_back({a, i});
-        }
-        
-    }
-    for(int i = 0;i < p;i++){
-        for(int j = 0;j < k;j++){
-            check();
-        }
-    }
 
     return 0;
 }
--- solve2.cpp
+++ solve2.cpp
@@ -1,43 +1,15 @@
-#include <bits/stdc++.h>
-#define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-#define INF 0x3f3f3f3f
-using namespace std;
-typedef long long ll;
-const int N=105,MaxW=100005;
-int v[N],w[N];
-int dp[MaxW];
-
-void Knapsack(int n,int W){
-
-    for(int i=1;i<=n;i++){ // 遍歷每個物品
-    
-        for(int j=W;j>0;j--){ // 從大到小遍歷重量
-        
-            if(j>=w[i]) dp[j]=max(dp[j],dp[j-w[i]]+v[i]);
-        }
-    }
+#include <cstdio>
+int fact(double n, int p){
+    if(p == 0) return 1;
+    else return fact(n, p - 1) * n;
 }
-void reKnapsack(int n,int W){
 
-    for(int j=W;j>0;j--){ // 從大到小遍歷重量
-    
-        for(int i=1;i<=n;i++){ // 遍歷每個物品
-        
-            if(j>=w[i]) dp[j]=max(dp[j],dp[j-w[i]]+v[i]);
-        }
-    }
-}
 int main(){
-    IOS
-    dp[0] = 0;
-    v[1] = 15, v[2] = 20, v[3] = 30;
-    w[1] = 1, w[2] = 3, w[3] = 4;
-    Knapsack(3, 4);
-    for(int i = 0;i <= 4;i++) cout << dp[i] << ' ';
-    cout << '\n';
-    memset(dp, 0, sizeof(dp));
-    reKnapsack(3, 4);
-    for(int i = 0;i <= 4;i++) cout << dp[i] << ' ';
+    double a;
+    int p;
+    scanf("%lf %d", &a, &p);
+    printf("%lf %d", a, p);
+    // printf("%lf", fact(a, p));
 
     return 0;
 }
