1c72925fe4d40b7edb4de076fe8d5b93c74d55c3
WuMaxtw <WuMax13@gmail.com>
2023-06-02 10:38:05 +0800

    OJ

--- solve.cpp
+++ solve.cpp
@@ -2,8 +2,15 @@
 #define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 using namespace std;
 typedef long long ll;
+pair<int, int> arr[200005];
 int main(){
     IOS
+    int n;
+    cin >> n;
+    for(int i = 0 ;i < n;i++){
+        cin >> arr[i].first >> arr[i].second;
+    }
+    
 
     return 0;
 }
