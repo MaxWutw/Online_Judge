7a9ffabec1ed3149b89f2d96cb1035c1f9d1dace
WuMaxtw <WuMax13@gmail.com>
2023-06-16 23:27:08 +0800

    OJ

--- solve.cpp
+++ solve.cpp
@@ -2,24 +2,14 @@
 #define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 using namespace std;
 typedef long long ll;
-ll c[1005], p[1005];
+ll h[105], s[105], k[105];
 int main(){
     IOS
-    ll n, x;
+    int n, x;
     cin >> n >> x;
-    for(int i = 0;i < n;i++) cin >> c[i];
-    for(int i = 0;i < n;i++) cin >> p[i];
-    ll dp[100005];
-    for(int i = 0;i <= x;i++){
-        dp[i] = 0;
-    }
-    for(int i = 0;i < n;i++){
-        for(int j = x;j >= 0;j--){
-            if(j - c[i] >= 0)
-                dp[j] = max(dp[j], dp[j - c[i]] + p[i]);
-        }
-    }
-    cout << dp[x] << '\n';
+    for(int i = 0;i < n;i++) cin >> h[i];
+    for(int i = 0;i < n;i++) cin >> s[i];
+    for(int i = 0;i < n;i++) cin >> k[i];
 
     return 0;
 }
