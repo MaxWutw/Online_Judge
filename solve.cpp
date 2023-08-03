8e6b1e0693f9ced7bb14b8d15a4a0e007192fbd2
WuMaxtw <WuMax13@gmail.com>
2023-08-03 22:49:45 +0800

    OJ

--- solve.cpp
+++ solve.cpp
@@ -1,9 +1,14 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+// #include <ext/pb_ds/detail/standard_policies.hpp>
 #define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 #define INF 0x3f3f3f3f
+#define lowbit(x) x&(-x)
 using namespace std;
+using namespace __gnu_pbds;
 typedef long long ll;
-const int N = 5e5 + 5;
+const int N = 2e5 + 5;
 int main(){
     IOS
 
