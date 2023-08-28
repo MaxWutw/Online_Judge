f619d6e03866d0395e37066feefab56870f39f2b
WuMax <WuMax13@gmail.com>
2023-08-28 23:18:17 +0800

    OJ

--- solve.cpp
+++ solve.cpp
@@ -7,36 +7,9 @@ using namespace std;
 using namespace __gnu_pbds;
 typedef long long ll;
 const int N = 2e5 + 5;
-vector<int> v;
-ll dc(vector<int> &vec, int l, int r){
-    if(vec.size() <= 2) return 0;
-    ll mid = (l + r) >> 1, dis = 0;
-    vector<int> small, large;
-    map<int, int> m;
-    for(auto e : vec){
-        if(e <= mid){
-            dis += m.size();
-            small.push_back(e);
-        }
-        else{
-            large.push_back(e);
-            if(m.count(e)) m.erase(e);
-            else m[e] = 1;
-        }
-    }
-    dis += dc(small, l, mid);
-    dis += dc(large, mid + 1, r);
-    return dis;
-}
 int main(){
     IOS
-    int n;
-    cin >> n;
-    for(int i = 1;i <= 2 * n;i++){
-        int tmp;
-        cin >> tmp, v.push_back(tmp);
-    }
-    cout << dc(v, 1, 2 * n) << '\n';
+    
 
     return 0;
 }
--- zerojudge/zj_f315.cpp
+++ zerojudge/zj_f315.cpp
@@ -1,3 +1,4 @@
+// divide and conquer
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
