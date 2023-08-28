53982da5925a167f82917eec555536d0cc884c49
WuMaxtw <WuMax13@gmail.com>
2023-08-28 23:16:12 +0800

    OJ

--- solve.cpp
+++ solve.cpp
@@ -5,40 +5,40 @@
 #define INF 0x3f3f3f3f
 #define int long long
 #define lowbit(x) x&(-x)
+#define L(x) (x << 1)
+#define R(x) ((x << 1) | 1)
 using namespace std;
 using namespace __gnu_pbds;
 typedef long long ll;
 const int N = 1e5 + 5;
-int bit[N + 5];
-int query(int idx){
-    int ret = 0;
-    for(int i = idx;i > 0;i-=lowbit(i)){
-        ret += bit[i];
+int n, q, arr[N];
+struct Node{
+    int right, left, mx, sum, ans;
+}seg[N * 4 + 5];
+void build(int pos, int l, int r){
+    if(l == r){
+        seg[pos] = {arr[l], arr[l], arr[l], arr[l]};
+        return;
     }
-    return ret;
+    int mid = (l + r) >> 1;
+    build(L(pos), l, mid);
+    build(R(pos), mid + 1, r);
+    modify(seg[pos], seg[L(pos)], seg[R(pos)]);
 }
-void update(int idx){
-    for(int i = idx;i <= N;i+=lowbit(i)){
-        bit[i]++;
-    }
+void modify(int pos, int l, int r, int idx, int val){
+
+}
+void query(int pos, int l, int r, int ql, int qr){
+
 }
 signed main(){
     IOS
-    int n, cnt = 1;
-    while(cin >> n && n){
-        int ans = 0, arr[N];
-        memset(bit, 0, sizeof(bit));
-        vector<int> b;
-        for(int i = 0;i < n;i++) cin >> arr[i], b.push_back(arr[i]);
-        sort(b.begin(), b.end());
-        b.resize(unique(b.begin(), b.end()) - b.begin());
-        for(int i = 0;i < n;i++)
-            arr[i] = lower_bound(b.begin(), b.end(), arr[i]) - b.begin() + 1;
-        for(int i = 0;i < n;i++){
-            ans += (i - query(arr[i]));
-            update(arr[i]);
-        }
-        cout << "Case #" << cnt++ << ": " << ans << '\n';
+    cin >> n >> q;
+    for(int i = 1;i <= n;i++) cin >> arr[i];
+    while(q--){
+        int a, b;
+        cin >> a >> b;
+        cout << query(1, 1, n, a, b);
     }
 
     return 0;
