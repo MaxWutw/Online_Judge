600d94a374d88afb3541768196f9aa77c469173e
WuMax <WuMax13@gmail.com>
2023-09-10 23:49:01 +0800

    OJ

--- solve2.cpp
+++ solve2.cpp
@@ -1,38 +1,87 @@
 #include <bits/stdc++.h>
 #define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 #define INF 0x3f3f3f3f
+#define MAXN 200005
+#define F first
+#define S second
 using namespace std;
-typedef long long ll;
-ll n, m, k, arr[(ll)5e5 + 5], block[(ll)5e5 + 5];
-ll query(int l, int r){
-    ll ans = -1;
-    for(int i = l;i <= r;i++){
-        if(i / k != l / k) break;
-        ans = max(ans, arr[i]);
-    }
-    for(int i = r;i >= l;i--){
-        if(i / k != r / k) break;
-        ans = max(ans, arr[i]);
-    }
-    for(int i = l / k + 1;i < r / k;i++){
-        ans = max(ans, block[i]);
-    }
-    return ans;
+int n, m;
+int in[MAXN], out[MAXN], D[MAXN];
+pair<int, int> ST[30][MAXN];
+vector<int> G[MAXN];
+
+struct timestamp {
+	int tt, node, dep;
+};
+
+vector<timestamp> ett;
+
+void init() {
+	for (int i = 0; i <= n; i++)
+		G[i].clear();
+}
+
+void dfs(int i, int dep) {
+	in[i] = ett.size();
+	ett.push_back({ett.size(), i, dep});
+	D[i] = dep;
+	
+	for (int e: G[i]) {
+		dfs(e, dep + 1);
+		ett.push_back({ett.size(), i, dep});
+	}
+	
+	out[i] = ett.size();
+	ett.push_back({ett.size(), i, dep});
+}
+
+void build() {
+	for (int i = 0; i < ett.size(); i++) {
+		ST[0][i] = {ett[i].dep, ett[i].node};
+	}
+	
+	for (int i = 1; i <= __lg(ett.size()); i++) {
+		for (int j = 0; j < ett.size(); j++) {
+			ST[i][j] = min(ST[i - 1][j], ST[i - 1][j + (1 << (i - 1))]);
+		}
+	}
+}
+
+pair<int, int> query(int l, int r) {
+	int lg = __lg(r - l + 1);
+	int range = 1 << lg;
+	pair<int, int> res(INF, INF);
+	
+	res = min(ST[lg][l], ST[lg][r - range + 1]);
+	return res;
+}
+
+void sol() {
+	cin >> n >> m;
+	init();
+	
+	for (int i = 1, a; i <= n; i++) {
+		while (cin >> a && a)
+			G[i].push_back(a);
+	}
+	
+	dfs(1, 1);
+	build();
+	
+	while (m--) {
+		int a, b;
+		cin >> a >> b;
+		
+		if (in[a] > in[b])
+			swap(a, b);
+		
+		auto res = query(in[a], in[b]);
+		cout << res.second << " " << D[a] + D[b] - 2 * D[res.S] << endl;
+	}
 }
 int main(){
     IOS
-    memset(block, 0, sizeof(block));
-    cin >> n;
-    for(int i = 0;i < n;i++) cin >> arr[i];
-    k = sqrt(n);
-    for(int i = 0;i < n;i++) block[i / k] = max(block[i / k], arr[i]);
-    cin >> m;
-    while(m--){
-        int l, r;
-        cin >> l >> r;
-        if(r < l) swap(l, r);
-        cout << query(--l, --r) << '\n';
-    }
+    sol();
 
     return 0;
 }
