d2c1098d81abb1237066b1f494a38fc532b7347f
WuMaxtw <WuMax13@gmail.com>
2023-10-03 13:49:33 +0800

    OJ

--- solve.cpp
+++ solve.cpp
@@ -1,43 +1,34 @@
 #include <bits/stdc++.h>
-// #include <ext/pb_ds/assoc_container.hpp>
-// #include <ext/pb_ds/tree_policy.hpp> 
 #define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 #define INF 0x3f3f3f3f
 #define int long long
-#define lowbit(x) x&(-x)
-#define L(x) (x << 1)
-#define R(x) ((x << 1) | 1)
 using namespace std;
-// using namespace __gnu_pbds;
 typedef long long ll;
-const int N = 1e5 + 5;
-int n, m, sz[N], p[N], ans[N];
-pair<int, int> query[N];
-int Find(int a){
-    if(a == p[a]) return a;
-    return p[a] = Find(p[a]);
-}
+int n, m, arr[1000][1000];
+int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
 signed main(){
     IOS
     cin >> n >> m;
-    for(int i = 1;i <= n;i++){
-        sz[i] = 1;
-        p[i] = i;
-    }
-    memset(ans, 0, sizeof(ans));
-    for(int i = 1;i <= m;i++) cin >> query[i].first >> query[i].second;
-    ans[m] = (n * (n - 1)) >> 1;
-    for(int i = m;i > 1;i--){
-        int a = Find(query[i].first), b = Find(query[i].second);
-        if(a == b) ans[i - 1] = ans[i];
-        else{
-            ans[i - 1] = ans[i] - sz[a] * sz[b];
-            p[b] = a;
-            sz[a] += sz[b];
-            sz[b] = 0;
+    for(int i = 0;i < n;i++){
+        string str;
+        cin >> str;
+        for(int j = 0;j < m;j++){
+            if(str[j] == '.') arr[i][j] = 0;
+            else arr[i][j] = 1;
         }
     }
-    for(int i = 1;i <= m;i++) cout << ans[i] << '\n';
+    queue<pair<int, int> > q;
+    dq.push({0, 0});
+    while(!q.empty()){
+        auto cur = q.front();
+        if(arr[cur.first][cur.second] == 1){
+            for(int i = 0;i < 4;i++){
+                if(cur.first + dx[i] >= 0 && cur.second + dy[i] >= 0 && cur.first + dx[i] < n && cur.second + dy[i] < m){
+                    if(arr[cur.first + dx[i]][cur.second + dy[i]])
+                }
+            }
+        }
+    }
 
     return 0;
 }
--- test.c
+++ test.c
@@ -1,18 +1,9 @@
 #include <stdio.h>
-<<<<<<< HEAD
-#include <string.h>
+#include <stdint.h>
 int main(){
-    char str[10];
-    scanf("%s", str);
-    printf("%c%c%c", str[1], str[0], str[2]);
-=======
-int main(){
-    int a =27.63;
-    int b = 35.45;
-    int sum = 0;
-    sum = a + b;
-    printf("%d + %d = %i\n", a, b, sum);
->>>>>>> 551706c745002c84d6bdecf311a578bc14a4704c
+	float a = 0.3;
+	if(a == 0.3) printf("a");
+	else printf("%.10f", a);
 
     return 0;
 }
